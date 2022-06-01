8e21c4bb81f49d85fc20cdc6b62867f553dafa78
Zach <shaw.zacht@gmail.com>
2022-05-31 23:49:42 -0700

    commit tree 7fe2b50d531fe01ab8a7c91874d85ebdf18ec15c

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+      '<h2>Hola <em>World</em>!</h2>'
+    end
+  
+  end
+
+   
+   
+    # class App < Sinatra::Base
+
+    #     get '/' do
+    #       'reload please!!!'
+    #     end
+        
+    #   end
